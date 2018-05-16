9c7c3ade8b69adeea78fbe785dd67bfc57797ff4
FlappyPumpkinPie <y119911@126.com>
2018-05-16 20:04:17 +0800

    原来如此

--- src/git/Employee.java
+++ src/git/Employee.java
@@ -2,8 +2,6 @@ package git;
 
 public class Employee {
 	public static void main(String[] args) {
-		System.out.println("居然是包名没有用;结尾,简直了");
-		System.out.println("111111111111111111");
-		System.out.println("2222222222222222");
+		System.out.println("明白了,pull是双向的,可以选择");
 	}
 }
