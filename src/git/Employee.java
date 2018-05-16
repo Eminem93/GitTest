1779d9fc822b0c9da8f83a4cea51ce1dd4e69628
Eminem93 <1987396204@qq.com>
2018-05-16 19:38:57 +0800

    Update Employee.java

--- src/git/Employee.java
+++ src/git/Employee.java
@@ -4,5 +4,6 @@ public class Employee {
 	public static void main(String[] args) {
 		System.out.println("居然是包名没有用;结尾,简直了");
 		System.out.println("111111111111111111");
+		System.out.println("2222222222222222");
 	}
 }
